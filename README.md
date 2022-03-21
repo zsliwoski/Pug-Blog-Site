2b577302d868d5a10d0b6ff207e731e4d15338c7
zarek <zsliwoski@gmail.com>
2022-03-20 23:17:27 -0600

    Creating site

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Final website for CS401
 
-Name: YOUR NAME HERE
-email: YOUR EMAIL HERE
+Name: Zarek Sliwoski
+Email: zareksliwoski@u.boisestate.edu
 
 ## How to run this website
-
+- run the database_setup.sql file to setup the database
 - npm install
 - node ./bin/www
 
 Browse to localhost:3000
+Begin using the site, creating and updating blog records
 
 ## Bugs
-
-Detail any known bugs here
+User is able to post empty titled entries
+User is able to post entries without content
+User has static authentication with the database.
 
 ## Features
 
-Review all the features of your app here and note anything that is missing or
-not implemented.
+This application features standard CRUD features written in javascript. It utilizes a MySQL back-end database and
+supports api functionality written through node.
+
+It utilizes the provided ".pug" template framework, and
+boils down to standard reusable html.
+
+## Future Improvements
+
+This app could be improved by providing additional styling as well as a way to create new users with their own blog posts. I think a good next improvement would see a user bio as well as a profile image.
+
+Hopefully this provides a solid basis moving forward for future site ideas/projects.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,147 @@
 var express = require('express');
+var mysql = require('mysql2');
 var router = express.Router();
 
+//create db connection creds
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "poster",
+  password: "password123", //would be nicer in real production :)
+  database: "cs_webdev_final"
+});
+
+//MYSQL DATABASE FUNCTIONS
+//post blogpost to db
+function createBlogPost(formData, onFinish){
+  var stmt = `INSERT INTO blog_posts (blog_title, blog_body) VALUES ('${formData.blog_title}','${formData.blog_body}')`;
+
+  con.query(stmt, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    onFinish();
+  });
+}
+
+//update blogpost in db
+function updateBlogPost(formData,onFinish){
+  var stmt = `UPDATE blog_posts SET blog_title = '${formData.blog_title}', blog_body = '${formData.blog_body}' WHERE id = ${formData.id}`;
+  con.query(stmt, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+    onFinish();
+  });
+}
+
+//drop blogpost from db
+function deleteBlogPost(id, onFinish){
+  var stmt = `DELETE FROM blog_posts WHERE id = ${id}`;
+  con.query(stmt, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    onFinish();
+  }); 
+}
+
+function getBlogPost(id, onFinish){
+  var stmt = `SELECT * FROM blog_posts WHERE id = ${id}`;
+
+  con.query(stmt, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    onFinish(result);
+  }); 
+}
+
+function getAllBlogPosts(onFinish){
+  var stmt = `SELECT * FROM blog_posts`;
+
+  con.query(stmt, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    onFinish(result);
+  }); 
+}
+
+//ROUTING FUNCTIONS
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CS401 - Introduction To Web Development' });
+  getAllBlogPosts((retVal)=>{
+    res.render('home', { title: 'Home', blog_posts: retVal });
+  });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  getAllBlogPosts((retVal)=>{
+    res.render('home', { title: 'Home', blog_posts: retVal });
+  });
+});
+
+/* GET edit page. */
+router.post('/edit', function(req, res, next) {
+
+  const entryToEdit = req.body;
+
+  getBlogPost(entryToEdit.id,(postData)=>{  
+    res.render('edit', 
+          { title: 'Edit Post', 
+          id: postData[0].id, 
+          blog_title: postData[0].blog_title, 
+          blog_body: postData[0].blog_body
+        });});
+});
+
+/* GET deletion page. */
+router.post('/delete', function(req, res, next) {
+
+  const entryToDelete = req.body;
+
+  getBlogPost(entryToDelete.id,(postData)=>{  
+      res.render('delete', 
+            { title: 'Delete Post', 
+            id: postData[0].id, 
+            blog_title: postData[0].blog_title, 
+            blog_body: postData[0].blog_body
+          });});
+});
+
+/* GET new post page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'New Post' });
+});
+
+//API FUNCTIONS
+/* POST new blog entry.*/
+router.post('/finish_post',function(req,res){
+  const blog_entry = req.body;
+
+  console.log(blog_entry);
+
+  createBlogPost(blog_entry, ()=>{
+    res.redirect(302, '/home');
+  });
+});
+
+/* PUT edit blog entry.*/
+router.post('/finish_edit',function(req,res){
+  const blog_entry = req.body;
+
+  console.log(blog_entry);
+
+  updateBlogPost(blog_entry,()=>{
+    res.redirect(302, '/home');
+  });
+});
+
+/* DELETE blog entry.*/
+router.post('/finish_delete',function(req,res){
+  const blog_id = req.body.id;
+
+  deleteBlogPost(blog_id,()=>{
+    res.redirect(302, '/home');
+  });
+});
+
+
 
 module.exports = router;
